68273ae2872642d7c08aa3a01d47a6721b3a187a
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2023-01-03 00:01:52 +0300

    updated the server

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
 
   // fetch the data from the server
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://chat-gpt-clone-nhin.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
